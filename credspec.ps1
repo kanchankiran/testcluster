12068f3d5ae06c7b577fea4102ab8e179eafbfd4
kanchankiran <39770427+kanchankiran@users.noreply.github.com>
2018-10-31 13:51:43 +0530

    Add files via upload

---
+++ credspec.ps1
@@ -0,0 +1,15 @@
+﻿
+
+Import-Module ActiveDirectory
+Get-ADServiceAccount -Identity 09492DSDev4211W 
+Install-ADServiceAccount -Identity 09492DSDev4211W 
+Test-AdServiceAccount -Identity 09492DSDev4211W 
+
+$url= "https://github.com/MicrosoftDocs/Virtualization-Documentation/blob/live/windows-server-container-tools/ServiceAccounts/CredentialSpec.psm1"
+$output = "C:\CredentialSpec.psm1"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri $url -OutFile $output
+
+Import-Module C:\CredentialSpec.psm1
+
+New-CredentialSpec -Name 09492DSDev4211W -AccountName 09492DSDev4211W
